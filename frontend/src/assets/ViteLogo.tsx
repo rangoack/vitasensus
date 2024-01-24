a0977bb5462ff51d7ea22998496bf6ddefcf05a0
Rango Ack <rangoack@proton.me>
2024-01-24 17:24:47 +0800

    update contract ABI and logo

--- frontend/src/assets/ViteLogo.tsx
+++ frontend/src/assets/ViteLogo.tsx
@@ -3,16 +3,19 @@ type Props = {
 };
 
 const ViteLogo = ({ className }: Props) => (
-	<svg viewBox="-6 -3 78 64" className={className}>
-		<g className="stroke-current" strokeWidth="0" fill="none" fillRule="evenodd">
-			<g transform="translate(-375.000000, -21.000000)" className="fill-current	">
-				<g transform="translate(375.000000, 21.000000)">
-					<g id="header">
-						<path d="M19.6153846,10.3846154 L17.6605322,60 L0,14.8070785 L19.6153846,10.3846154 Z M63.4615385,4.33431069e-13 L18.4615385,59.423077 L24.2649743,9.2374494 L63.4615385,4.33431069e-13 Z"></path>
-					</g>
-				</g>
-			</g>
-		</g>
+	<svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M35.52 29.2925L18.2241 16.7413C12.1692 12.3503 4.22381 18.8747 7.38532 25.6387C15.2429 42.4473 24.6997 62.7255 28.083 70.2221L39.6845 45.3363C42.3191 39.6827 40.5765 32.9602 35.5153 29.2879L35.52 29.2925Z" fill="url(#paint0_linear_1_17)" />
+		<path d="M76.6608 12.2352L88.5442 37.5173C91.1834 43.1294 89.39 49.8243 84.2965 53.3767L35.649 87.3258C32.6123 89.4453 28.7252 86.1877 30.2874 82.8425L63.2891 12.249C65.933 6.59086 74.0031 6.58164 76.6608 12.2352Z" fill="url(#paint1_linear_1_17)" />
+		<defs>
+			<linearGradient id="paint0_linear_1_17" x1="14.1613" y1="46.1795" x2="38.4538" y2="32.1064" gradientUnits="userSpaceOnUse">
+				<stop stopColor="#00BEFF" />
+				<stop offset="1" stopColor="#00FF95" />
+			</linearGradient>
+			<linearGradient id="paint1_linear_1_17" x1="19.0465" y1="66.5269" x2="85.7586" y2="27.8906" gradientUnits="userSpaceOnUse">
+				<stop stopColor="#00BEFF" />
+				<stop offset="1" stopColor="#00FF95" />
+			</linearGradient>
+		</defs>
 	</svg>
 );
 
