de51440b23cc28d1abeee3cd167ebbdc5eece521
Rango Ack <rangoack@proton.me>
2024-05-23 22:16:20 +0800

    update

--- README.md
+++ README.md
@@ -7,7 +7,15 @@ git submodule update --init --recursive
 
 # install deps 
 npm --prefix ./frontend install
-npm --prefix ./vitasensus-op install 
+npm --prefix ./vitasensus-operator-server install 
+
+# build and run api
+cd frontend
+npm run build:api
+npm run start
+
+# run frontend
+npm run debug
 
 # run unit test for test/*.spec.ts
 npx vuilder test
--- frontend/src/components/ProposalsList.tsx
+++ frontend/src/components/ProposalsList.tsx
@@ -9,7 +9,8 @@ const ProposalsList = () => {
 	const space = useCurrentSpace();
 	const [maxProposalCount, setMaxProposalsCount] = useState(10);
 
-	const { data: proposals } = useProposals(space?.id, maxProposalCount);
+	let { data: proposals } = useProposals(space?.id, maxProposalCount);
+	proposals = proposals?.filter((x) => x.id !== 5)
 
 	const updateCount = useCallback(() => {
 		if ((proposals?.length ?? 0) >= maxProposalCount) setMaxProposalsCount(maxProposalCount + 10);
